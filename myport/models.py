70aabdd03559d59664d5eb3c3242909c61850123
Mind Health <142549908+mindhealth14@users.noreply.github.com>
2024-01-08 14:35:58 +1000

    Update models.py

--- myport/models.py
+++ myport/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 2619a3918320ef852ef3d49de35081a6e110ccb9
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -33,12 +30,12 @@ class Project(models.Model):
     image = models.ImageField(upload_to='projects/images', blank=True)
     image_url = models.URLField(max_length=500, blank=True)
     embed_code = models.CharField(max_length=550, blank=True)
-<<<<<<< HEAD
+
     git_source_code = models.CharField(max_length=300, blank=True)
-=======
+
     website_url = models.URLField(max_length=500, blank=True)
     github = models.URLField(max_length=500, blank=True)
->>>>>>> 2619a3918320ef852ef3d49de35081a6e110ccb9
+
     date = models.DateField(auto_now=True)
     
     class Meta:
@@ -56,10 +53,7 @@ class Project(models.Model):
     def get_absolute_url(self):
         return reverse('myport:project_detail', args=[self.id, self.slug])
     
-<<<<<<< HEAD
-=======
 
->>>>>>> 2619a3918320ef852ef3d49de35081a6e110ccb9
     
 
        
